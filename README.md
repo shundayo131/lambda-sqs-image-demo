b83ef4df5579ed7f9d0c7cd2d8f7fe0eab531ab8
shundayo131 <shundayo.131@gmail.com>
2025-07-26 14:12:51 -0700

    deployed S3 bucket and SQS queue for image processing

--- README.md
+++ README.md
@@ -13,32 +13,3 @@ The `cdk.json` file tells the CDK Toolkit how to execute your app.
 * `npx cdk diff`    compare deployed stack with current state
 * `npx cdk synth`   emits the synthesized CloudFormation template
 
-## Architecture 
-
-[1] User Uploads Image
-        │
-        ▼
-+----------------------+
-|  S3: Source Image    |  <-- Raw image goes here
-+----------------------+
-        │
-        │  (S3 event notification)
-        ▼
-+----------------------+
-|     SQS Queue        |  <-- Decouples processing
-+----------------------+
-        │
-        │  (Event Source for Lambda)
-        ▼
-+----------------------------+
-| Lambda: imageProcessor     |
-|  - Downloads original      |
-|  - Resizes via Sharp       |
-|  - Uploads thumbnails      |
-|  - Stores metadata         |
-+----------------------------+
-        │         │
-        ▼         ▼
-+----------------+  +----------------------+
-| S3: Thumbnails |  | DynamoDB: Metadata   |
-+----------------+  +----------------------+
--- lib/lambda-sqs-image-demo-stack.ts
+++ lib/lambda-sqs-image-demo-stack.ts
@@ -1,16 +1,32 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class LambdaSqsImageDemoStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // Create an S3 bucket to upload images 
+    const imageBucket = new s3.Bucket(this, 'ImageUploadBucket', {
+      removalPolicy: cdk.RemovalPolicy.DESTROY, // delete bucket when stack is destroyed. Dev only. 
+      autoDeleteObjects: true,
+    })
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'LambdaSqsImageDemoQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // Create an SQS queue to receive image upload jobs from S3 events
+    const imageQueue = new sqs.Queue(this, 'ImageProcesssingQueue', {
+      queueName: 'ImageJobQueue', // specify a name for the queue to make it easier to identify in AWS Console 
+    })
+
+    // Output the bucket name
+    new cdk.CfnOutput(this, 'ImageBucketName', {
+      value: imageBucket.bucketName,
+    }) 
+
+    // Output the queue URL 
+    new cdk.CfnOutput(this, 'ImageQueueArn', {
+      value: imageQueue.queueArn,
+    })
+
   }
 }
