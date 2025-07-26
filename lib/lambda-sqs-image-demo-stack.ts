c580a8c8c266fed4700ee79c132a8ed8ddbd3719
shundayo131 <shundayo.131@gmail.com>
2025-07-26 14:29:40 -0700

    added imageQueue resource policy to allow S3 to send messages to SQS

--- lib/lambda-sqs-image-demo-stack.ts
+++ lib/lambda-sqs-image-demo-stack.ts
@@ -2,6 +2,7 @@ import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as s3 from 'aws-cdk-lib/aws-s3';
 import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as iam from 'aws-cdk-lib/aws-iam';
 
 export class LambdaSqsImageDemoStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
@@ -17,6 +18,19 @@ export class LambdaSqsImageDemoStack extends cdk.Stack {
     const imageQueue = new sqs.Queue(this, 'ImageProcesssingQueue', {
       queueName: 'ImageJobQueue', // specify a name for the queue to make it easier to identify in AWS Console 
     })
+
+    // Grant the S3 bucket permission to send messages to the SQS queue 
+    imageQueue.addToResourcePolicy(new iam.PolicyStatement({
+      effect: iam.Effect.ALLOW, // allow the action 
+      principals: [new iam.ServicePrincipal('s3.amazonaws.com')], // only S3 can send messages to this queue
+      actions: ['sqs:SendMessage'], // allow S3 to send messages to the queue
+      resources: [imageQueue.queueArn], // restrict to this queue only
+      conditions: { 
+        ArnEquals: {
+          'aws:SourceArn': imageBucket.bucketArn, // restrict to this bucket only
+        }
+      }
+    }));
 
     // Output the bucket name
     new cdk.CfnOutput(this, 'ImageBucketName', {
